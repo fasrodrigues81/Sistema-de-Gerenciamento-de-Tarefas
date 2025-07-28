06d6817471d8a1c0612af7b512e91441842f8e05
fasrodrigues81 <fasrodrigues81@gmail.com>
2025-07-28 18:46:59 -0300

    Camada dto

---
+++ sgp/src/main/java/com/treina/recife/sgp/dto/UsuarioDto.java
@@ -0,0 +1,83 @@
+package com.treina.recife.sgp.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.treina.recife.sgp.constants.StatusUsuario;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UsuarioDto {
+
+    private long userId;
+
+    private String nome;
+
+    private String cpf;
+
+    private String email;
+
+    private String senha;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
+
+    private StatusUsuario status;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public StatusUsuario getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusUsuario status) {
+        this.status = status;
+    }
+
+}
